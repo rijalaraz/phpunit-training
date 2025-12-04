a4a05c1b49028b245dc5e3eccf344982a6c0050e
Rija Lalaina <rrazafindrabe@meilleurtaux.com>
2025-12-04 15:30:28 +0300

    004 Easily add a new test method using the test fixture

--- tests/queue/Queue004Test.php
+++ tests/queue/Queue004Test.php
@@ -19,29 +19,6 @@ final class Queue004Test extends TestCase
         unset($this->queue);
     }
 
-    public function testNewQueueIsEmpty() 
-    {
-        $this->assertEquals(0, $this->queue->getCount());
-    }
-
-    public function testAnItemIsAddedToTheQueue()
-    {
-        $this->queue->push('green');
-
-        $this->assertEquals(1, $this->queue->getCount());
-    }
-
-    public function testAnItemIsRemovedFromTheQueue()
-    {
-        $this->queue->push('green');
-
-        $item = $this->queue->pop();
-
-        $this->assertEquals(0, $this->queue->getCount());
-
-        $this->assertEquals('green', $item);
-    }
-
     public function testAnItemIsRemovedFromTheFrontOfTheQueue()
     {
         $this->queue->push('first');
