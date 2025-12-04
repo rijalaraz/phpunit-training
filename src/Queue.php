f90f805c9cef2fe26b42d8dcbdf5944879ac0ade
Rija Lalaina <rrazafindrabe@meilleurtaux.com>
2025-12-04 15:15:37 +0300

    004 Easily add a new test method using the test fixture

--- src/Queue.php
+++ src/Queue.php
@@ -33,7 +33,7 @@ class Queue
      */
     public function pop()
     {
-        return array_pop($this->items);
+        return array_shift($this->items);
     }
 
     /**
---
+++ tests/queue/Queue004Test.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Queue;
+
+use PHPUnit\Framework\TestCase;
+use Src\Queue;
+
+final class Queue004Test extends TestCase
+{
+    protected $queue;
+
+    protected function setUp(): void
+    {
+        $this->queue = new Queue();
+    }
+
+    protected function tearDown(): void
+    {
+        unset($this->queue);
+    }
+
+    public function testNewQueueIsEmpty() 
+    {
+        $this->assertEquals(0, $this->queue->getCount());
+    }
+
+    public function testAnItemIsAddedToTheQueue()
+    {
+        $this->queue->push('green');
+
+        $this->assertEquals(1, $this->queue->getCount());
+    }
+
+    public function testAnItemIsRemovedFromTheQueue()
+    {
+        $this->queue->push('green');
+
+        $item = $this->queue->pop();
+
+        $this->assertEquals(0, $this->queue->getCount());
+
+        $this->assertEquals('green', $item);
+    }
+
+    public function testAnItemIsRemovedFromTheFrontOfTheQueue()
+    {
+        $this->queue->push('first');
+        $this->queue->push('second');
+
+        $this->assertEquals('first', $this->queue->pop());
+    }
+}
