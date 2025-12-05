051d71a3755a1d3de682cf11ecc26f08612a42f7
Rija Lalaina <rrazafindrabe@meilleurtaux.com>
2025-12-05 14:19:57 +0300

    001 Test doubles_ create mock objects to remove dependencies on external resources

---
+++ tests/test_doubles/Mailer001Test.php
@@ -0,0 +1,18 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Src\Mailer;
+
+final class Mailer001Test extends TestCase
+{
+    public function testMock()
+    {
+        $mock = $this->createMock(Mailer::class);
+
+        $mock->method('sendMessage')->willReturn(true);
+
+        $result = $mock->sendMessage('dave@example.com', 'Hello');
+
+        $this->assertTrue($result);
+    }
+}
