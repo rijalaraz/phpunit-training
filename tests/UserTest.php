688f6376776e964f6eeba00371c9b1ce0a2b5e9d
Rija Lalaina <rrazafindrabe@meilleurtaux.com>
2025-12-05 19:46:43 +0300

    004 Customise the creation of the mock object_ the getMockBuilder method

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -53,4 +53,27 @@ final class UserTest extends TestCase
 
         $this->assertTrue($user->notify('Hello'));
     }
+
+    public function testCannotNotifyUserWithNoEmail()
+    {
+        $user = new User();
+
+        $mock_mailer = $this->getMockBuilder(Mailer::class)
+            ->onlyMethods([])
+            ->getMock()
+        ;
+
+        // $mock_mailer = $this->createMock(Mailer::class);
+
+        // $mock_mailer
+        //     ->method('sendMessage')
+        //     ->will($this->throwException(new \Exception()))
+        // ;
+
+        $user->setMailer($mock_mailer);
+
+        $this->expectException(\Exception::class);
+
+        $user->notify('Hello');
+    }
 }
